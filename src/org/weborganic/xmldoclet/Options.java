c757b7396248dd38b99dcddb65bef68914eddefc
tyenki <tyenki@localhost>
2012-04-30 14:13:29 +1000

    Added @override annotations, removed redundant doc, code formatting

--- src/org/weborganic/xmldoclet/Options.java
+++ src/org/weborganic/xmldoclet/Options.java
@@ -2,7 +2,7 @@
  * This file is part of the Weborganic XMLDoclet library.
  *
  * For licensing information please see the file license.txt included in the release.
- * A copy of this licence can also be found at 
+ * A copy of this licence can also be found at
  *   http://www.opensource.org/licenses/artistic-license-2.0.php
  */
 package org.weborganic.xmldoclet;
@@ -20,7 +20,7 @@ import com.sun.tools.doclets.Taglet;
 
 /**
  * Container for the options for the XML Doclet.
- * 
+ *
  * @author Christophe Lauret
  *
  * @version 21 June 2010
@@ -30,7 +30,7 @@ public final class Options {
   /**
    * An empty array constant for reuse.
    */
-  private static final String[] EMPTY_ARRAY = new String[]{}; 
+  private static final String[] EMPTY_ARRAY = new String[]{};
 
   /**
    * The default encoding for the output
@@ -44,14 +44,14 @@ public final class Options {
 
   /**
    * Determines whether the output is a single file or multiple files.
-   * 
+   *
    * Populated from the command line via the "-multiple" flag.
    */
   private boolean multipleFiles = false;
 
   /**
    * Determines the directory where output is placed.
-   * 
+   *
    * Populated from the command line via the "-d [directory]" flag.
    */
   private File directory;
@@ -72,7 +72,7 @@ public final class Options {
   private String implementsFilter = null;
 
   /**
-   * Filter classes with the specified annotation.  
+   * Filter classes with the specified annotation.
    */
   private String annotationFilter = null;
 
@@ -82,8 +82,8 @@ public final class Options {
   private Map<String, Taglet> taglets = new HashMap<String, Taglet>();
 
   /**
-   * Name of the file - used for single output only.  
-   * 
+   * Name of the file - used for single output only.
+   *
    * Populated from the command line via the "-filename [file]" flag.
    */
   private String filename = DEFAULT_FILENAME;
@@ -110,7 +110,7 @@ public final class Options {
 
   /**
    * Returns the charset to use to encode the output.
-   * 
+   *
    * @return the charset to use to encode the output.
    */
   public Charset getEncoding() {
@@ -119,7 +119,7 @@ public final class Options {
 
   /**
    * Returns the directory where to store the files.
-   * 
+   *
    * @return where to store the files.
    */
   public File getDirectory() {
@@ -128,29 +128,29 @@ public final class Options {
 
   /**
    * Returns the name of the file for single output.
-   * 
+   *
    * @return the name of the file for single output.
    */
   public String getFilename() {
     return this.filename;
   }
-  
+
   /**
    * Returns the taglet instance for the specified tag name.
-   * 
+   *
    * @param name The name of the tag.
    * @return The corresponding <code>Taglet</code> or <code>null</code>.
    */
   public Taglet getTagletForName(String name) {
     for (String n : this.taglets.keySet()) {
-      if (n.equals(name)) return this.taglets.get(n); 
+      if (n.equals(name)) return this.taglets.get(n);
     }
     return null;
   }
 
   /**
    * Indicates whether these options specify a filter.
-   * 
+   *
    * @return <code>true</code> if the class must implement or extends or have a specific annotation.
    *         <code>false</code> otherwise.
    */
@@ -159,11 +159,11 @@ public final class Options {
   }
 
   /**
-   * Filters the included set of classes by checking whether the given class matches the 
+   * Filters the included set of classes by checking whether the given class matches the
    * '-extends', '-implements' and '-annotated' options.
-   * 
+   *
    * @param doc the class documentation.
-   * @return <code>true</code> if the class should be included; <code>false</code> otherwise. 
+   * @return <code>true</code> if the class should be included; <code>false</code> otherwise.
    */
   public boolean filter(ClassDoc doc) {
     // Extends
@@ -177,7 +177,7 @@ public final class Options {
       for (ClassDoc i : interfaces) {
         if (this.implementsFilter.equals(i.toString())) return true;
       }
-      return false; 
+      return false;
     }
     // Annotation
     if (this.annotationFilter != null) {
@@ -185,7 +185,7 @@ public final class Options {
       for (AnnotationDesc i : annotations) {
         if (this.annotationFilter.equals(i.annotationType().qualifiedName())) return true;
       }
-      return false; 
+      return false;
     }
 
     // No filtering
@@ -201,13 +201,13 @@ public final class Options {
 
   /**
    * A JavaDoc option parsing handler.
-   * 
+   *
    * <p>This one returns the number of arguments required for the given option.
-   * 
+   *
    * @see com.sun.javadoc.Doclet#optionLength(String)
-   * 
+   *
    * @param option The name of the option.
-   * 
+   *
    * @return The number of arguments for that option.
    */
   public static int getLength(String option) {
@@ -228,7 +228,7 @@ public final class Options {
 
   /**
    * Retrieve the expected options from the given array of options.
-   * 
+   *
    * @param root The root object which contains the options to be retrieved.
    */
   public static Options toOptions(String options[][], DocErrorReporter reporter) {
@@ -267,7 +267,7 @@ public final class Options {
       }
     }
 
-    // Extends 
+    // Extends
     if (has(options, "-filename")) {
       String name = get(options, "-filename");
       if (name != null && !o.multipleFiles) {
@@ -276,7 +276,7 @@ public final class Options {
       } else reporter.printWarning("'-filename' option ignored");
     }
 
-    // Extends 
+    // Extends
     if (has(options, "-extends")) {
       String superclass = get(options, "-extends");
       if (superclass != null) {
@@ -285,7 +285,7 @@ public final class Options {
       } else reporter.printWarning("'-extends' option ignored - superclass not specified");
     }
 
-    // Annotated 
+    // Annotated
     if (has(options, "-annotated")) {
       String annotation = get(options, "-annotated");
       if (annotation != null) {
@@ -294,7 +294,7 @@ public final class Options {
       } else reporter.printWarning("'-annotated' option ignored - annotation not specified");
     }
 
-    // Implements 
+    // Implements
     if (has(options, "-implements")) {
       String iface = get(options, "-implements");
       if (iface != null) {
@@ -327,10 +327,10 @@ public final class Options {
 
   /**
    * Indicates whether the specified option is defined.
-   * 
+   *
    * @param options the matrix of command line options.
    * @param name    the name of the requested option.
-   * 
+   *
    * @return <code>true</code> if defined; <code>false</code> otherwise.
    */
   private static boolean has(String[][] options, String name) {
@@ -342,10 +342,10 @@ public final class Options {
 
   /**
    * Returns the single value for the specified option if defined.
-   * 
+   *
    * @param options the matrix of command line options.
    * @param name    the name of the requested option.
-   * 
+   *
    * @return the value if available or <code>null</code>.
    */
   private static String get(String[][] options, String name) {
@@ -355,12 +355,12 @@ public final class Options {
 
   /**
    * Finds the options array for the specified option name.
-   * 
+   *
    * <p>The first element is <i>always</i> the name of the option.
-   * 
+   *
    * @param options the matrix of command line options.
    * @param name    the name of the requested option.
-   * 
+   *
    * @return the corresponding array or an empty array.
    */
   private static String[] find(String[][] options, String name) {
