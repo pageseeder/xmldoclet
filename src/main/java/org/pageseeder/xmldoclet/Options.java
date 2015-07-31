8244d56f071ed9f2a0b3ad087f41aaa8ff377432
Christophe Lauret <clauret@weborganic.com>
2015-07-31 16:29:56 +1000

    Updated package name

--- src/main/java/org/pageseeder/xmldoclet/Options.java
+++ src/main/java/org/pageseeder/xmldoclet/Options.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.weborganic.xmldoclet;
+package org.pageseeder.xmldoclet;
 
 import java.io.File;
 import java.lang.reflect.Method;
