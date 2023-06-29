272deea881df9ef03b311b2fc90824177284a338
christophe <tyenki@gmail.com>
2023-06-29 10:59:50 +1000

    Add sample block taglet for testing

--- src/test/java/org/example/SampleBase.java
+++ src/test/java/org/example/SampleBase.java
@@ -1,5 +1,12 @@
 package org.example;
 
+/**
+ * This is a sample base class.
+ *
+ * <p>Here is an inline taglet example {@glossary base_class}.</p>
+ *
+ * @error-id 1234 An error ID
+ */
 public abstract class SampleBase implements SampleInterface {
 
   protected final String something;
---
+++ src/test/java/org/pageseeder/xmldoclet/tags/ErrorIdTaglet.java
@@ -0,0 +1,52 @@
+package org.pageseeder.xmldoclet.tags;
+
+import com.sun.source.doctree.DocTree;
+import com.sun.source.doctree.UnknownBlockTagTree;
+import com.sun.source.doctree.UnknownInlineTagTree;
+import jdk.javadoc.doclet.Taglet;
+import org.pageseeder.xmldoclet.Markup;
+import org.pageseeder.xmldoclet.Options;
+
+import javax.lang.model.element.Element;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Set;
+
+public class ErrorIdTaglet implements Taglet {
+
+  @Override
+  public Set<Location> getAllowedLocations() {
+    return EnumSet.allOf(Location.class);
+  }
+
+  @Override
+  public boolean isInlineTag() {
+    return false;
+  }
+
+  @Override
+  public String getName() {
+    return "error-id";
+  }
+
+  public String toString(UnknownBlockTagTree tag) {
+    String text = tag.getContent().toString();
+    int x = text.indexOf(' ');
+    if (x >= 0) {
+      return "<error id=\""+text.substring(0, x)+"\">"+text.substring(x).trim()+"</error>";
+    } else {
+      return "<error>"+text+"</error>";
+    }
+  }
+
+  @Override
+  public String toString(List<? extends DocTree> tags, Element element) {
+    StringBuilder out = new StringBuilder();
+    for (DocTree tag : tags) {
+      if (tag instanceof UnknownBlockTagTree) {
+        out.append(toString((UnknownBlockTagTree)tag));
+      }
+    }
+    return out.toString();
+  }
+}
