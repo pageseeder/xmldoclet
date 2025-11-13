2fa1ad662664113e8a6a1e730e3a61714c700a1b
clauret <clauret@weborganic.com>
2025-11-13 15:10:31 +1100

    Chore: Add JavaDoc for `DocletException` and mark `element` field as transient

--- src/main/java/org/pageseeder/xmldoclet/DocletException.java
+++ src/main/java/org/pageseeder/xmldoclet/DocletException.java
@@ -4,9 +4,20 @@ import org.eclipse.jdt.annotation.Nullable;
 
 import javax.lang.model.element.Element;
 
+/**
+ * This exception is thrown to indicate an error related to the processing of a doclet element.
+ *
+ * <p>It serves as a wrapper for issues that occur during operations involving doclet elements,
+ * such as generation of documentation or processing metadata.
+ *
+ * @author Christophe Lauret
+ *
+ * @version 1.0
+ * @since 1.0
+ */
 public class DocletException extends Exception {
 
-  private final @Nullable Element element;
+  private final transient @Nullable Element element;
 
   public DocletException(@Nullable Element element, String message) {
     super(message);
